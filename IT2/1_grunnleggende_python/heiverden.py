2156cdcb79cf2d9e0e05b93e149462b5ab58b2ac
uwohlen <112326477+uwohlen@users.noreply.github.com>
2025-09-10 14:02:04 +0200

    gfdgh

--- IT2/1_grunnleggende_python/heiverden.py
+++ IT2/1_grunnleggende_python/heiverden.py
@@ -4,3 +4,17 @@ print("livet","går","videre",sep="---")
 print()
 print("jepp")
 
+
+
+import random as rd
+i = rd.randint(1,10)
+while i < 5:
+  if i == 0:
+    print("Null")
+    continue
+  print(i)
+  i = rd.randint(1,10)
+
+  
+
+print("hei")
