108facb43d893ed90513a1d06c7eb6995bc47ecb
uwohlen <112326477+uwohlen@users.noreply.github.com>
2025-10-08 13:09:25 +0200

    python vist i timen

---
+++ IT2/1_grunnleggende_python/test_fb.py
@@ -0,0 +1,8 @@
+def bytt(liste:list,indeks:int,fra_hva,til_hva) -> list:
+  """
+  Bytter ut en verdi i en todimensjonal tabell basert på indeks pr rad
+  """
+  for rad in liste:
+    if rad[indeks] == fra_hva:
+      rad[indeks] = til_hva
+  return fra_hva
