941fac40412d11cb02969c64d5c3bfcbda5cdee2
uwohlen <112326477+uwohlen@users.noreply.github.com>
2025-10-08 12:16:47 +0200

    python funksjonsbibliotek

--- IT2/1_grunnleggende_python/funksjonsbibliotek.py
+++ IT2/1_grunnleggende_python/funksjonsbibliotek.py
@@ -2,16 +2,24 @@ def bytt(tabell:list,indeks:int,fra_verdi,til_verdi) -> list:
   """
     Går gjennom en todimensjonal liste, og bytter ut en verdi i hver rad basert på indeks og fra_verdi
 
-    Inn: 
+    Parametre: 
       tabell: list - en todimensjonal struktur av "rader" og "kolonner"
       indeks: int  - hvilken "kolonne" som skal sjekkes i hver "rad"
       fra_verdi    - hvilken verdi som skal byttes ut, kan være tekst, tall, ...
       til_verdi    - hvilken verdi det skal byttes til, kan være tekst, tall, ...
 
-    Ut:
-      returnerer en liste med samme struktur som originalen, men med enkelte verdier byttet ut
+    Return:
+      en liste med samme struktur som originalen, men med enkelte verdier byttet ut
   """
+  temp = []
+  # lager en kopi
   for rad in tabell:
+    temp.append([])
+    for kol in rad:
+      temp[-1].append(kol)
+  # gjør endringer i kopien
+  for rad in temp:
     if rad[indeks] == fra_verdi:
       rad[indeks] = til_verdi
-  return tabell
+  
+  return temp
