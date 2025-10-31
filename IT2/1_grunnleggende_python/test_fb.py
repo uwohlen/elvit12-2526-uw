b7053dee37b7dd1a0e406c2ee55e1b828523d0f7
uwohlen <112326477+uwohlen@users.noreply.github.com>
2025-10-31 15:18:34 +0100

    IT2 fil-referanser i mapper

--- IT2/1_grunnleggende_python/test_fb.py
+++
@@ -1,9 +0,0 @@
-def bytt(liste:list,indeks:int,fra_hva,til_hva) -> None:
-  """
-  Bytter ut en verdi i en todimensjonal tabell basert på indeks pr rad
-  """
-  for rad in liste:
-    if rad[indeks] == fra_hva:
-      rad[indeks] = til_hva
-
-
