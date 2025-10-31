4d0c1c371c064424272006c57d16a56471ddafaa
uwohlen <112326477+uwohlen@users.noreply.github.com>
2025-10-31 15:42:08 +0100

    Update oppg8.py

--- IT2/Skriftlig eksamen/V26/oppg8/oppg8.py
+++ IT2/Skriftlig eksamen/V26/oppg8/oppg8.py
@@ -1,14 +1,23 @@
 import sys
 import os
+print()
 print("Filen ligger her:",sys.path[0])
+print()
 print("Mappa over filen (parent):",os.path.dirname(sys.path[0]))
+print()
 print("Mappa over mappa over filen (grandparent):",os.path.dirname(os.path.dirname(sys.path[0])))
+# osv...
 
+# Legger til en referanse til biblioteksmappa, slik at filene der finnes av programmet
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]),"bibliotek"))
+print()
 print("Funksjonsbiblioteket:",sys.path[-1])
+print()
 
+import funksjon_bibliotek as fb # VSC reagerer på denne måten å skrive det på (men det virker!)
 
-import funksjon_bibliotek as fb
+# VSC ville ha skrevet det slik (men det virker ikke!):
+#from ..bibliotek import funksjon_bibliotek as fb 
 
 liste = [['M', 178], ['K', 176], ['K', 171], ['K', 163], ['M', 187], ['K', 170], ['M', 172], ['K', 168], ['M', 169], ['K', 161], ['K', 174], ['M', 206], ['M', 180], ['K', 165], ['K', 180], ['K', 174], ['K', 167], ['K', 170], ['K', 177], ['M', 193], ['M', 188], ['K', 162], ['K', 165], ['K', 166], ['M', 174], ['K', 177], ['K', 182], ['K', 171], ['K', 169], ['K', 174]]
 
