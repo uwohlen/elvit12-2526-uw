9bcda64a3bf182547a3a7b774c755a8a1a6c55bd
uwohlen <112326477+uwohlen@users.noreply.github.com>
2025-10-31 14:25:11 +0100

    Update bruke_funk_bib_i_mappe.py

--- IT2/1_grunnleggende_python/bruke_funk_bib_i_mappe.py
+++ IT2/1_grunnleggende_python/bruke_funk_bib_i_mappe.py
@@ -1,12 +1,4 @@
-import sys
-import os
-print(sys.path)
-
-sys.path.append(os.path.join(sys.path[0],"uw_bibliotek"))
-
-print(sys.path)
-
-import uw_funksjonsbibliotek as fb
+from uw_bibliotek import uw_funksjonsbibliotek as fb
 
 
 liste = [['M', 178], ['K', 176], ['K', 171], ['K', 163], ['M', 187], ['K', 170], ['M', 172], ['K', 168], ['M', 169], ['K', 161], ['K', 174], ['M', 206], ['M', 180], ['K', 165], ['K', 180], ['K', 174], ['K', 167], ['K', 170], ['K', 177], ['M', 193], ['M', 188], ['K', 162], ['K', 165], ['K', 166], ['M', 174], ['K', 177], ['K', 182], ['K', 171], ['K', 169], ['K', 174]]
