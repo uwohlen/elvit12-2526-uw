b7c65d4dde301ea848aa1e6149807ba6530787b0
uwohlen <112326477+uwohlen@users.noreply.github.com>
2026-02-04 12:24:50 +0100

    IT2 v24

--- IT2/Skriftlig eksamen/V25/oppg_8_datasett/les_csv.py
+++ IT2/Skriftlig eksamen/V25/oppg_8_datasett/les_csv.py
@@ -11,5 +11,10 @@ with open(filnavn,encoding="utf-8") as fil:
   for linje in hele_filen:
     alt.append(linje)
 
+
+for i in range(len(alt)):
+  for j in range(1,len(alt[i])):
+    alt[i][j] = int(alt[i][j])
+    
 print(overskrifter)
 print(alt)
--- IT2/Skriftlig eksamen/V25/oppg_8_datasett/les_json.py
+++ IT2/Skriftlig eksamen/V25/oppg_8_datasett/les_json.py
@@ -9,6 +9,7 @@ with open(filnavn, encoding="utf-8") as fil:
 
 sammen = []
 
+# Setter sammen linjer som har blitt delt pga. komma-tegn i teksten
 for info in data:
   sammen.append({"deltatt":""})
   for key,value in info.items():
@@ -17,6 +18,34 @@ for info in data:
       sammen[-1]["deltatt"] += value
     else:
       sammen[-1]["deltatt"] += "," + value
-  print(sammen[-1])
+  #print(sammen[-1])
+
+sammen.pop(0)
+keys = sammen.pop(0)
+
+keys["deltatt"] = keys["deltatt"].split(";")
+
+for i in range(1,len(keys["deltatt"])):
+  keys["deltatt"][i] = keys["deltatt"][i][24:-1]
+  if keys["deltatt"][i].count("-") > 0:
+    plass = keys["deltatt"][i].index("-")
+    keys["deltatt"][i] = keys["deltatt"][i][:plass-1]
+
+keys["deltatt"][0] = keys["deltatt"][0][1:-1]
+#print(keys)
+
+for i in range(len(sammen)):
+  sammen[i]["deltatt"] = sammen[i]["deltatt"].split(";")
+  for j in range(len(keys["deltatt"])):
+    if j == 0:
+      sammen[i][keys["deltatt"][j]] = sammen[i]["deltatt"][j][1:-1]
+    else:
+      sammen[i][keys["deltatt"][j]] = int(sammen[i]["deltatt"][j])
+  sammen[i].pop("deltatt")
 
 
+# Bare for å se hva jeg har:
+for info in sammen:
+  for key,value in info.items():
+    print(key, " : ", value)
+    #pass
